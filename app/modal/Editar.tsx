404af588608a54868404686a08c5d3d2938b1c0b
Ana Caroline <212008482@aluno.unb.br>
2025-02-23 16:42:31 -0300

    Fiz: Modais arrumados

--- app/modal/Editar.tsx
+++ app/modal/Editar.tsx
@@ -17,14 +17,33 @@ const Modal_editar: React.FC<ModalProps> = ({isVisible, onClose}) =>{
 
     return(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="flex flex-col m-10 items-center bg-green-300 w-1/2 h-3/4 p-5 rounded-xl">
-                <h2 className="text-xl mb-4" > Editar Local </h2>
+            <div className="flex flex-col m-10 items-center justify-center bg-green-700 w-1/2 h-50 p-5 rounded-xl">
+                <h2 className="text-xl text-black text-bold " > Editar Local </h2>
+            
+                <div className="justify-center">
+                <div className="flex flex-col mt-5 items-center">
+                    <h2 className="mr-4 ml-5 text-white">País</h2>
+                    <input className="ml-4 block w-full px-10 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                            placeholder="Selecione"></input>
+                </div>
+                
+                <div className="flex flex-col mt-5 items-center">
+                    <h2 className="mr-4 ml-5 text-white">Local</h2>
+                    <input className="ml-4 block w-full px-10 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                            placeholder="Digite o local que deseja conhecer"></input>
+                </div>
 
+                <div className="flex flex-col mt-5 items-center">
+                    <h2 className="mr-4 ml-5 text-white">Meta</h2>
+                    <input className="ml-4 block w-full px-10 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                            placeholder="Mês/Ano"></input>
+                </div>
                 
 
-                <div className="w-1/2 flex justify-center">
+                <div className="w-1/2 flex flexjustify-center items-center">
                     <button onClick={onClose} className="flex m-10 items-center px-6 py-3 bg-red-500 text-white rounded-md"> Cancelar </button>
                     <button onClick={onClose} className="flex m-10 items-center px-6 py-3 bg-blue-500 text-white rounded-md"> Editar </button>
+                </div>
                 </div>
             </div>
         </div>
--- app/modal/Excluir.tsx
+++ app/modal/Excluir.tsx
@@ -1,9 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
 import React from "react";
-import "react-quill/dist/quill.snow.css";
-
-const ReactQuill = dynamic(() => import('react-quill'), {ssr: false})
 
 interface ModalProps{
     isVisible2: boolean;
@@ -17,11 +14,11 @@ const Modal_excluir: React.FC<ModalProps> = ({isVisible2, onClose2}) =>{
 
     return(
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="flex flex-col m-10 items-center bg-green-300 w-1/2 h-3/4 p-5 rounded-xl">
-                <h2 className="text-xl mb-4" > Você tem certeza que quer excluir esse destino? </h2>
+            <div className="flex flex-col m-10 items-center bg-green-600 w-50 h-25 p-5 rounded-xl">
+                <h2 className="text-black text-center text-xl mb-4 justify-center text-bold" > Você tem certeza que quer excluir esse destino? </h2>
                 <div className="w-1/2 flex justify-center">
-                    <button onClick={onClose2} className="flex m-10 items-center px-6 py-3 bg-red-500 text-white rounded-md"> Não </button>
-                    <button onClick={onClose2} className="flex m-10 items-center px-6 py-3 bg-blue-500 text-white rounded-md"> Sim </button>
+                    <button onClick={onClose2} className="flex m-10 items-center px-10 py-5 bg-red-500 text-black text-bold rounded-md"> Não </button>
+                    <button onClick={onClose2} className="flex m-10 items-center px-10 py-5 bg-blue-500 text-black text-bold rounded-md"> Sim </button>
                 </div>
             </div>
         </div>
