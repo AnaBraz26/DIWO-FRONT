3c9d9104986d77482c987f12b3e3200b1542c7d6
Ana Caroline <212008482@aluno.unb.br>
2025-02-23 14:44:56 -0300

    Feat: Criando card dos paises

--- app/components/cardPaises.tsx
+++ app/components/cardPaises.tsx
@@ -1,29 +1,29 @@
-import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 
-interface cardPaisesProp{    
-    ImgSrc: string,
+interface cardPaisesProp{
     nome: string,
     local: string,
-    meta: string 
-
+    meta: string,
+    img: string,
 }
 
 const cardPaises: React.FC<cardPaisesProp> = ({
-    ImgSrc,
     nome,
     local,
-    meta }) => {
+    meta,
+    img }) => {
         return(
-            <article className="flex flex-col m-5 items-center bg-gray-200 w-fill p-5 rounded-xl">
-                <Image className="rounded-lg" 
-                    src = {ImgSrc}
-                    alt ={"profile picture"} 
-                    height={100}
-                    width={100}></Image>
+            <article className="flex flex-col m-5 bg-gray-200 w-fill p-5 rounded-xl">
+                <Image className="rounded-lg"
+                       src = {img}
+                       alt = {"bandeira"}
+                       height = {100}
+                       width = {100}></Image>
                 <h1 className="text-x1 font-medium mt-2 mb-2">{nome}</h1>
                 <h2 className="text-black/70">{local}</h2>
                 <h2 className="text-black/70">{meta}</h2>
-            </article>    
+            </article>
         )
     }
+
+    export default cardPaises;
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,50 @@
+"use client";
 import Image from "next/image";
 import { Formik, Form, Field } from "formik";
+import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
+import InputMask from "react-input-mask-next";
+
 
 export default function Home(){
-  return <main>
+
+  return (
     <div>
       <NavBar></NavBar>
       
+      {/* ------------------------------------------------ */}
+          
+      {/* Formulário de Seleção e Edição */}
+      <div className="flex justify-center items-center bg-green-600 p-20">
+        
+        <div className="flex flex-col w=1/3ml-4">
+          <h2 className="mr-4 ml-5 text-white">País</h2>
+          <input className="ml-4 block w-full px-3 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                 placeholder="Selecione"></input>
+        </div>
+        
+        <div className="flex flex-col w-1/3 ml-4">
+        <h2 className="mr-4 ml-5 text-white">Local</h2>
+        <input className="ml-4 block w-full px-3 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                 placeholder="Digite o local que deseja conhecer"></input>
+        </div>
+
+        <div className="flex flex-col w=1/3 ml-4">
+        <h2 className="mr-4 ml-5 text-white">Meta</h2>
+        <input className="ml-4 block w-full px-3 py-2 border bg-white shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md"
+                 placeholder="Mês/Ano"></input>
+        </div>
+
+        <div className="flex flex-col ml-4 w=1/3">
+        <button className="mt-6 ml-4 px-9 py-3 border bg-green-900 shadow-sm focus:ring-indigo-500focus:border-indigo-500 sm:text-sm rounded-md">
+          Adicionar</button>
+        </div>      
+      </div>
+
       <div>
         
       </div>
-      
-      <div className="">
 
-      
-      </div>
-    
     </div>
-
-  </main>
+  );
 }
